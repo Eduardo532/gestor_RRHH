6fd3bd5f55d20a4b7f0fafd8aace8eab7df37328
JavierRm136 <j.riquelme30@ufromail.cl>
2025-10-26 23:11:33 -0300

    agreglo de version android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.gestor_empleados"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.gestor_empleados"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1"
 
