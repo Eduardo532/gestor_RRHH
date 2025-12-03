2aaf1ff3b6ccc72a68b26e5ab0dd5196a08438fd
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2025-12-03 11:34:29 -0300

    commit

--- app/src/test/java/com/example/gestor_empleados/viewmodel/LoginViewModelTest.kt
+++ app/src/test/java/com/example/gestor_empleados/viewmodel/LoginViewModelTest.kt
@@ -25,31 +25,23 @@ class LoginViewModelTest {
 
     @Test
     fun `login exitoso actualiza estado a loginExitoso true`() = runTest {
-        // 1. GIVEN (Dado que): El repositorio responde éxito
+
         coEvery { authRepository.login(any(), any()) } returns Result.success(Unit)
+        val rutPrueba = "11.111.111-1"
+        val emailEsperado = "11111111-1@gestor.app"
+        val password = "password"
 
-        // Inicializamos el ViewModel inyectando el mock (Nota: necesitarás refactorizar el VM para aceptar el repo en el constructor, ver nota abajo*)
-        viewModel = LoginViewModel(authRepository)
 
-        // 2. WHEN (Cuando): Llamamos a login
-        viewModel.login("11.111.111-1", "password")
-
-        // 3. THEN (Entonces): El estado debe ser exitoso
-        assertTrue(viewModel.uiState.value.loginExitoso)
-        assertEquals(false, viewModel.uiState.value.isLoading)
-    }
-
-    @Test
-    fun `login fallido actualiza estado con error`() = runTest {
-        // 1. GIVEN: El repositorio falla
-        coEvery { authRepository.login(any(), any()) } returns Result.failure(Exception("Error de red"))
+        coEvery {
+            authRepository.login(eq(emailEsperado), eq(password))
+        } returns Result.success(Unit)
 
         viewModel = LoginViewModel(authRepository)
 
-        // 2. WHEN
-        viewModel.login("11.111.111-1", "password")
+        viewModel.login(rutPrueba,password)
 
-        // 3. THEN
-        assertEquals("RUT o contraseña incorrectos.", viewModel.uiState.value.error)
-    }
+
+        assertTrue(viewModel.uiState.value.isloginSuccessful)
+
+        assertEquals(false, viewModel.uiState.value.isLoading)    }
 }
